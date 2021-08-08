59ee8c40617d06861ce6311b97bbe9183b8c527e
Vladimir Ventura <vladventura@outlook.es>
2021-08-08 17:30:12 -0400

    Some changes to account for long names

--- src/components/ScrollText.jsx
+++ src/components/ScrollText.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import "./ScrollText.css";
 
-const ScrollText = ({ text, showText }) => (
-  <div className="scroll-container">
-    {showText && <p className="scrollable-name">{text}</p>}
-  </div>
-);
+const ScrollText = ({ text, showText }) => {
+  const [longString, setLongString] = useState(false);
+
+  useEffect(() => {
+    if (text.length >= 30) setLongString(true);
+  }, [text]);
+
+  return (
+    <div className={`scroll-container${longString ? " animated-child" : ""}`}>
+      {showText && (
+        <p className={`scrollable-name${longString ? " animated-name" : ""}`}>
+          {text}
+        </p>
+      )}
+    </div>
+  );
+};
 
 export { ScrollText };
