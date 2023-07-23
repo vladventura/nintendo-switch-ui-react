148478836a50e9d1e7e210e18265f375246fe951
Vladimir Ventura <vladventura@outlook.es>
2023-07-23 19:39:15 -0400

    Adding imports, context use, and activate/deactivate
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- src/components/UtilsRow.jsx
+++ src/components/UtilsRow.jsx
@@ -1,8 +1,12 @@
 import './UtilsRow.css';
-import { MessageIcon } from "../icons";
 import { faBagShopping, faImage, faGamepad, faGear, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { faMessage } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Util } from './Util';
+import { useControls } from '../hooks/controls';
+import { useContext, useEffect } from 'react';
+import { InputContext } from '../context/inputContext';
+
+const activeSetIndex = 1;
 
 const icons = [
     ["messages", faMessage],
@@ -15,11 +19,33 @@ const icons = [
 
 
 export const UtilsRow = () => {
-    return <div className='rosso'>
-        {icons.map((i, n) => 
-        <div className="util-icon">
-            <FontAwesomeIcon key={"icon"+n} className={`icon ${i[0]}`} icon={i[1]} />
-        </div>
+    const context = useContext(InputContext);
+    const activeSet = context.state.activeSet;
+    const [onKeyDown, selectedIndex, setSelectedIndex, activate, deactivate] = useControls(icons.length);
+
+    useEffect(() => {
+        if (activeSet === activeSetIndex) {
+            activate();
+        } else {
+            deactivate();
+        }
+    }, [activeSet]);
+
+    return <div
+        onKeyDown={onKeyDown}
+        onScroll={(e) => e.preventDefault()}
+        onScrollCapture={(e) => e.preventDefault()}
+        onClick={(e) => e.preventDefault()}
+        className='utils-row'>
+        {icons.map((i, index) =>
+            <Util
+                key={"util" + index}
+                iconClass={i[0]}
+                faIcon={i[1]}
+                onClick={() => setSelectedIndex(index)}
+                tabIndex={index}
+                active={selectedIndex === index}
+            />
         )}
     </div>
 }
