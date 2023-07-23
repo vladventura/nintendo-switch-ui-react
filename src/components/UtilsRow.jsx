e1224134694c89cb7b6a4b69ffcd21f698235a51
Vladimir Ventura <vladventura@outlook.es>
2023-07-23 00:20:33 -0400

    Adding bottom bar
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- src/components/UtilsRow.jsx
+++ src/components/UtilsRow.jsx
@@ -1,5 +1,6 @@
 import './UtilsRow.css';
 import { MessageIcon } from "../icons";
+import { faBagShopping, faImage, faGamepad, faGear, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { faMessage } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -7,10 +8,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const UtilsRow = () => {
     return <div className='rosso'>
         <div className="util-icon">
-            <MessageIcon className="icon" />
+            <FontAwesomeIcon className='icon messages' icon={faMessage} />
         </div>
         <div className="util-icon">
-            <FontAwesomeIcon className='icon' icon={faMessage} />
+            <FontAwesomeIcon className='icon shop' icon={faBagShopping} />
+        </div>
+        <div className="util-icon">
+            <FontAwesomeIcon className='icon gallery' icon={faImage} />
+        </div>
+        <div className="util-icon">
+            <FontAwesomeIcon className='icon' icon={faGamepad} />
+        </div>
+        <div className="util-icon">
+            <FontAwesomeIcon className='icon' icon={faGear} />
+        </div>
+        <div className="util-icon">
+            <FontAwesomeIcon className='icon' icon={faPowerOff} />
         </div>
     </div>
 }
