85d0c986135a1ca43618f00f8f8ab4d0d10864c3
Vladimir Ventura <vladventura@outlook.es>
2023-07-23 22:40:09 -0400

    Removing unneeded useEffect
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- src/components/GameCarousel.jsx
+++ src/components/GameCarousel.jsx
@@ -2,23 +2,15 @@ import "./GameCarousel.css";
 import { Game } from "./Game";
 import { games } from "../mock/";
 import { useControls } from "../hooks/controls";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { InputContext } from "../context/inputContext";
 
 const activeSetIndex = 0;
 
 const GameCarousel = () => {
-  const context = useContext(InputContext);
-  const activeSet = context.state.activeSet;
-  const [onKeyDown, selectedIndex, setSelectedIndex, activate, deactivate, controlActive] = useControls(games.length);
+  const {activeSet} = useContext(InputContext);
+  const [onKeyDown, selectedIndex, setSelectedIndex] = useControls(games.length);
 
-  useEffect(() => {
-    if (activeSet === activeSetIndex) {
-      activate();
-    } else {
-      deactivate();
-    }
-  }, [activeSet]);
 
   return (
     <div className="content" onKeyDown={onKeyDown}>
@@ -37,7 +29,7 @@ const GameCarousel = () => {
               image={game.image}
               key={game.name}
               tabIndex={index}
-              active={selectedIndex === index && controlActive}
+              active={selectedIndex === index && activeSet === activeSetIndex}
             />
           );
         })}
