4a89d4ff7280d90be4aee97e720480a8cd4e2405
Vladimir Ventura <vladventura@outlook.es>
2023-08-21 14:02:16 -0400

    Adding light theme and small fixes. Z-index was inevitable
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -2,22 +2,22 @@ import "./BottomBar.css";
 import { NintendoSwitch } from "../icons"
 
 export const BottomBar = () => {
-    return <div className="bottom-bar">
+    return <div>
         <hr />
         <div className="under-line-container">
             <NintendoSwitch className="nintendo-switch-icon" />
             <div className="console-button">
-                <p className="console-button-shape">
-                    +
-                </p>
+                <div className="console-button-shape">
+                    <p>+</p>
+                </div>
                 <p className="console-button-text">
                     Options
                 </p>
             </div>
             <div className="console-button">
-                <p className="console-button-shape">
-                    A
-                </p>
+                <div className="console-button-shape">
+                    <p>A</p>
+                </div>
                 <p className="console-button-text">
                     Confirm
                 </p>
